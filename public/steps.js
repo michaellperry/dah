1a3a44e8018b6613d25f10c4a06d85cdc9d240d2
Michael L Perry <michael@qedcode.com>
2016-05-12 23:00:22 -0500

    Walk through steps.

---
+++ public/steps.js
@@ -0,0 +1,31 @@
+var steps = [
+    {
+        instructions: 'With Jinaga, you record historical facts as JSON documents. Express a fact that represents you as a user. You want your fact to be unique, so you should use a random number. I\'ve generated one for you. *',
+        example:
+            'var user = {'               + '\n' +
+            '    type: \'Jinaga.User\',' + '\n' +
+            '    identity: -7164293'     + '\n' +
+            '};'                         + '\n' +
+            'j.fact(user);',
+        footnote: '* If you are pairing with a friend, be sure not to use your friend\'s number!',
+        expectation: function (facts, variables) {
+            if (facts.some(function (fact) {
+                return fact.hasOwnProperty('type') && fact.type == 'Jinaga.User' &&
+                    fact.hasOwnProperty('identity');
+            })) {
+                return { matched: true };
+            }
+            else {
+                return { matched: false, message: 'I didn\'t see a fact for you as a user.' };
+            }
+        }
+    },
+    {
+        instructions: 'So that\'s it. Go forth and build something cool.',
+        example: 'npm install jinaga',
+        footnote: '',
+        expectation: function (facts, variables) {
+            return { matched: false };
+        }
+    }
+];
--- public/walkthrough.js
+++ public/walkthrough.js
@@ -11,18 +11,18 @@ var vm = new (function () {
         this.variables = {};
         
         this.eval = function (code) {
-            var variableNames = findVariableNames(code);
-            
             var preamble = "";
             for (var name in this.variables) {
                 preamble += "var " + name + " = this.variables." + name + ";\n";
             }
             
             var post = "\n";
+            var variableNames = findVariableNames(code);
             variableNames.forEach(function (name) {
                 post += "this.variables." + name + " = " + name + ";\n";
             });
-            
+
+            sending = [];            
             eval(preamble + code + post);
             
             facts = facts.concat(sending);
@@ -40,12 +40,30 @@ var vm = new (function () {
         }
     })();
     
+    this.step = ko.observable(0);
+    this.instructions = ko.computed(function () {
+        return steps[this.step()].instructions;
+    }, this);
+    this.example = ko.computed(function () {
+        return steps[this.step()].example;
+    }, this);
+    this.footnote = ko.computed(function () {
+        return steps[this.step()].footnote;
+    }, this);
     this.code = ko.observable('');
     this.exception = ko.observable('');
     this.executeCode = function executeCode() {
         this.exception('');
         try {
             sandbox.eval(this.code());
+            var result = steps[this.step()].expectation(facts, sandbox.variables);
+            if (result.matched) {
+                this.step(this.step()+1);
+                this.code('');
+            }
+            else {
+                this.exception(result.message);
+            }
         }
         catch (x) {
             this.exception(x.message);
